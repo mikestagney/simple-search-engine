8b74f53338207cd7dd93997c21954a602770c126
mikestagney <mikestagney@yahoo.com>
2021-07-14 20:02:35 -0400

    first draft of stage 2

--- Main.java
+++ Main.java
@@ -1,21 +1,42 @@
 package search;
 
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of people:");
+        int number = Integer.parseInt(input.nextLine());
 
-        String firstLine = input.nextLine();
-        String word = input.nextLine();
+        ArrayList<String> people = new ArrayList<>();
+        System.out.println("Enter all people:");
+        for (int i = 0; i < number; i++) {
+            String line = input.nextLine();
+            people.add(line);
+        }
 
-        String result = "not found";
-        String[] array = firstLine.split(" ");
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(word)) {
-                result = Integer.toString(i + 1);
+        System.out.println("Enter the number of search queries:");
+        int searchNumber = Integer.parseInt(input.nextLine());
+
+        for (int i = 0; i < searchNumber; i++) {
+            System.out.println("Enter data to search people:");
+            String holder = input.nextLine();
+
+            StringBuilder result = new StringBuilder();
+
+            String temp = people.get(i);
+            if (temp.toLowerCase(Locale.ROOT).contains(holder.toLowerCase(Locale.ROOT))) {
+                result.append(temp);
+                result.append("\n");
+            }
+            if (result == null) {
+                System.out.println("No matching people found");
+            } else {
+                System.out.println(result);
             }
         }
-        System.out.println(result);
+
     }
 }
